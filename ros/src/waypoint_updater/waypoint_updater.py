0714737a7a791e5c193f7a02f850fcc70c4a2c0e
aijose <arun.jose@gmail.com>
2019-10-15 22:10:44 -0600

    Reduce frequency at which the final waypoints are published from 50 to 25 to avoid showing up behind the vehicle. This may be related to performance issues of the machine.

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -49,7 +49,7 @@ class WaypointUpdater(object):
         #rospy.spin()
 
     def loop(self):
-        rate = rospy.Rate(50)
+        rate = rospy.Rate(25)
         while not rospy.is_shutdown():
             if self.pose and self.base_waypoints:
                 # Get closest waypoint
